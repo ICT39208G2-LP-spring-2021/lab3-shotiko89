c5e770a2ffd084f1a99fd88fe56059a0115ebfd0
shotiko89 <tsurtsumia.shotiko@mail.ru>
2021-07-18 01:08:48 +0400

    lab3

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+function valid($search, $val){
+    $conn = new mysqli('localhost', 'root', '','gtu123');
+    $sql = "SELECT * FROM users where $search = '$val'";
+    $check = mysqli_query($conn, $sql);
+    if($res = mysqli_num_rows($check)>0){
+
+        $Err = 1;
+        return $Err;
+
+    }
+}
+?>
---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function (){
+    $('#registerButton').click(function (){
+        $('#registerForm').toggleClass('visually-hidden');
+    });
+    $('#errMsgs').click(function(event){
+       $('#errMsg').toggleClass('visually-hidden');
+    });
+});
